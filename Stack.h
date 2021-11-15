7f7f25cb4538efd7b93fb470f946e14a23a16eba
A1exxander <88210134+A1exxander@users.noreply.github.com>
2021-11-15 05:35:57 -0500

    Update Stack.h

--- Stack.h
+++ Stack.h
@@ -7,7 +7,7 @@
 class ArrayStack { // Reason why we use a stack is since we need to flip the input from little endian to big endian - Stacks are useful for reversing data and when getting the binary representation of a number with code, we get the last ( smallest number ) first 
 private:
     int top{};
-    std::array<int, 8> myArray;
+    std::array<int, 8> myArray; // Size 8 since we will only be representing 1 byte.
 public:
 
     ArrayStack();
